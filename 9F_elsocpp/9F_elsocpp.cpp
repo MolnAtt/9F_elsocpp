6373cf428bd41303a46d399f7a568b95fac85e9e
MolnAtt <molnar.attila@szlgbp.hu>
2021-09-03 10:03:26 +0200

    Add project files.

---
+++ 9F_elsocpp/9F_elsocpp.cpp
@@ -0,0 +1,45 @@
+// 9F_elsocpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/* asdfélkj */
+
+
+/** /
+    blokk-komment
+/**/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() // entry point
+{
+    cout << "Hello World!\n" << "es ez is " << "meg ez is " << endl << "ez is sorvege\n"<< "lam.";
+    int a = 5;
+    int b = 7;
+    cout << a + b;
+
+    int c;
+    cin >> c;
+
+    cout << "Te most azt mondtad, hogy " << c;
+
+    string d= "blabla";
+
+    cout << d;
+
+    bool e = 5<7;
+    
+    if (e)
+    {
+        cout << "öt kisebb, mint 7";
+    }
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        cout << "Lepek!\n";
+    }
+
+
+}
