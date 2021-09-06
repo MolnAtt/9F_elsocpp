9303ef2c0cea2b4b1d023b83e723564ef85bc385
MolnAtt <molnar.attila@szlgbp.hu>
2021-09-06 11:30:26 +0200

    while, for, switch

--- 9F_elsocpp/9F_elsocpp.cpp
+++ 9F_elsocpp/9F_elsocpp.cpp
@@ -35,11 +35,74 @@ int main() // entry point
     {
         cout << "öt kisebb, mint 7";
     }
+    else
+    {
+        cout << "olyan is van, hogy else";
+    }
 
     for (size_t i = 0; i < 5; i++)
     {
         cout << "Lepek!\n";
     }
 
-    cout << "izé";
+    for (unsigned int i = 0; i < 5; i++)
+    {
+        cout << "Lepek!\n";
+    }
+
+    cout << "izé\n";
+
+    unsigned int t = 1;
+
+    cout << t - 1 << endl;
+    cout << t - 2 << endl;
+    cout << t - 3 << endl;
+    cout << t - 4 << endl;
+
+    int s = 0;
+    while (s<5)
+    {
+        cout << s++ << endl;
+    }
+
+    int kapcsolo;
+    cin >> kapcsolo;
+
+    switch (kapcsolo)
+    {
+    case 2:
+        cout << "ez egy 2-es lett" << endl;
+        break;
+    case 3:
+        cout << "ez viszont egy 3-as lett" << endl;
+        break;
+    default:
+        cout << "ez valami egészen  más lett" << endl;
+        break;
+    }
+
+
+    cin >> kapcsolo;
+
+    // fall through-effektus, esetek összevonására hagyták benne! De egyébként mindig break-elni kell.
+    switch (kapcsolo)
+    {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        cout << "ez egy 5 vagy annál kisebb lett" << endl;
+        break;
+    case 6:
+        cout << "ez viszont 6 lett" << endl;
+        break;
+    default:
+        cout << "ez valami 6-nál nagyobb lett" << endl;
+        break;
+    }
+
+
+
 }
